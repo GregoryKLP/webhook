e9acf5fedab82d5cf9384545348954cdd11ce47f
GregoryKLP <133002832+GregoryKLP@users.noreply.github.com>
2024-09-23 23:07:58 +0300

    Update webhook-script.js

--- webhook-script.js
+++ webhook-script.js
@@ -1,12 +1,12 @@
 function sendData(data, endpoint) {
   if (document.location.href.search('appspot.com') == -1) {
     var xhr = new XMLHttpRequest();
-    
-    // Отправляем объект data напрямую, не сериализуя его в строку
+    var stringifiedData = JSON.stringify(data);
+
     xhr.open('POST', endpoint);
     xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(data)); // Преобразуем объект в JSON только на этапе отправки
-
+    xhr.send(stringifiedData);
+    
     xhr.onload = function () {
       if (xhr.status.toString()[0] !== '2') {
         console.error(xhr.status + '> ' + xhr.statusText);
