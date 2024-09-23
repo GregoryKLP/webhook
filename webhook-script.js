229fae5b1c48cf5526dbeb3b5736532e52122666
GregoryKLP <133002832+GregoryKLP@users.noreply.github.com>
2024-09-23 23:11:11 +0300

    Update webhook-script.js

--- webhook-script.js
+++ webhook-script.js
@@ -1,16 +1,13 @@
 function sendData(data, endpoint) {
-  if (document.location.href.search('appspot.com') == -1) {
-    var xhr = new XMLHttpRequest();
-    var stringifiedData = JSON.stringify(data);
+if (document.location.href.search('appspot.com') == -1) {
+var xhr = new XMLHttpRequest();
+  var stringifiedData = JSON.stringify(data);
 
-    xhr.open('POST', endpoint);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(stringifiedData);
-    
-    xhr.onload = function () {
-      if (xhr.status.toString()[0] !== '2') {
-        console.error(xhr.status + '> ' + xhr.statusText);
-      }
-    };
-  }
-}
+  xhr.open('POST', endpoint);
+  xhr.send(stringifiedData);
+  xhr.onload = function () {
+    if (xhr.status.toString()[0] !== '2') {
+      console.error(xhr.status + '> ' + xhr.statusText);
+    }
+  };
+} 
